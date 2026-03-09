ccb75401e9512847eeb28e130bcbbe25dd6784a8
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-03-09 18:26:01 +0545

    user redirect accroding to their role andmin and user

---
+++ admin/userredirect.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include "databaseconnection.php";
+if (isset($_POST['submit'])) {
+    $email = $_POST['user_email'];
+    $password = $_POST['user_password'];
+
+    $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        if ($row['userrole'] == 'admin') {
+            header("Location: ../admin/dashboard.php");
+            $_SESSION['user_email'] = $row['email'];
+            $_SESSION['user_role'] = $row['userrole'];
+        } elseif ($row['userrole'] == 'user') {
+            $_SESSION['user_email'] = $row['email'];
+            $_SESSION['user_role'] = $row['userrole'];
+            header("Location: ../files/index.php");
+        }
+    } else {
+        echo "<script>alert('Invalid email or password. Please try again.');</script>";
+    }
+}
+?>
