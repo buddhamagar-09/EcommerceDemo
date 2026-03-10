f0f09b4d328fa9e83e3b466e49867d08ee6f286e
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-03-10 12:32:57 +0545

    authentication added for users in index page

--- files/index.php
+++ files/index.php
@@ -1,3 +1,12 @@
+
+<?php
+session_start();
+if (!isset($_SESSION['user_email'])) {
+    header('location:../files/login.php');
+} elseif ($_SESSION['user_role'] == 'user') {
+    header('location:index.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
