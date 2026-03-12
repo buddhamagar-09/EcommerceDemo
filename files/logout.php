c5af37eb0efd4dc4f7434142a34046c4b74c31f6
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-03-12 11:56:41 +0545

    session destryoyed for user and admin after they click on logout

--- files/logout.php
+++ files/logout.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
-if (isset($_SESSION['user_email']) && isset($_SESSION['user_role']) && isset($_SESSION['user_name'])) {
-    session_destroy();
-    header("Location: login.php");
-} else {
-    header("Location: index.php");
-}
+
+$_SESSION = [];
+session_unset();
+session_destroy();
+
+header("Location: index.php");
+exit;
 ?>
