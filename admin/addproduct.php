64331004892230fc078725bc45f931f420771f1d
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-03-13 21:55:45 +0545

    product add backend logic

--- admin/addproduct.php
+++ admin/addproduct.php
@@ -19,5 +19,27 @@ if(isset($_POST['addproduct']))
         //     'error' = 0
         //     'size' = 2300000
         // }
+    if (move_uploaded_file($image_location,$upload_location))
+        {
+            $query = $conn ->prepare("Insert into products(name,description,price,quantity,image) values(?,?,?,?,?)");
+            $query ->bind_param("ssiis",$name,$description,$price,$quantity,$image);
+            $query ->execute();
+            if($query)
+            {
+                echo "<script>alert('Product added successfully');</script>";
+                echo "<script>window.location.href='productlist.php';</script>";
+            }
+            else
+            {
+                echo "<script>alert('Failed to add product');</script>";
+                echo "<script>window.location.href='addproduct.php';</script>";
+            }
+        }
+     
+    }
+    else
+    {
+        echo "<script>alert('Please fill all fields');</script>";
+        echo "<script>window.location.href='addproduct.php';</script>";
     }
 ?>
