8d749d2e1706e1f2c8aeac283f954e87c972563c
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-04-21 10:53:28 +0545

    update stock sql added

--- files/success.php
+++ files/success.php
@@ -73,13 +73,16 @@ $sql1 = "SELECT * FROM cart WHERE user_id = '$user_id'";
 $result1 = mysqli_query($conn, $sql1);
 
 while ($row = mysqli_fetch_assoc($result1)) {
-    $product_id = $row['product_id'];
-    $quantity = $row['quantity'];
+    $product_id = (int) $row['product_id'];
+    $quantity = (int) $row['quantity'];
     $price = $row['price'];
 
 
     $sql2 = "INSERT INTO order_items (order_id,product_id,quantity,price) VALUES ('$order_id','$product_id','$quantity','$price')";
     $result2 = mysqli_query($conn, $sql2);
+
+    $stock_sql = "UPDATE products SET quantity = quantity - $quantity WHERE id = $product_id AND quantity >= $quantity";
+    mysqli_query($conn, $stock_sql);
 }
 
 // clear cart items
